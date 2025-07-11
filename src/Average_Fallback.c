e29769f829cf6c1655be641ec27fdc1405742f62
Tess Isola <tessai159@icloud.com>
2025-07-11 09:22:10 -0500

    Minor renaming

--- src/Average_Fallback.c
+++ src/Average_Fallback.c
@@ -4,17 +4,17 @@
 #include "Color.h"
 
 struct Color lab_avg_fallback(struct Color *colors, uint16_t num_col) {
-	float total_l = 0.0f;
-	float total_a = 0.0f;
-	float total_b = 0.0f;
+	float l = 0.0f;
+	float a = 0.0f;
+	float b = 0.0f;
 	for (size_t i = 0; i < num_col; ++i) {
-	  total_l += colors[i].cielab.l;
-	  total_a += colors[i].cielab.a;
-	  total_b += colors[i].cielab.b;
+	  l += colors[i].cielab.l;
+	  a += colors[i].cielab.a;
+	  b += colors[i].cielab.b;
 	}
-	total_l /= (float)num_col;
-	total_a /= (float)num_col;
-	total_b /= (float)num_col;
-	/* a;lsk djf;alskdjf ;oaksdj fp;oiaeuhdpfiuh  */
-	return Color_create(0,0,0);
+	l /= (float)num_col;
+	a /= (float)num_col;
+	b /= (float)num_col;
+	
+	return Color_create_lab(l, a, b);
 }
