7a8eb49cb3fcb7665f8baf8f1a0aa6932f8330aa
Tess Isola <tessai159@icloud.com>
2025-07-15 14:50:33 -0500

    Set up cross-compiling for Windows

--- src/Average_Fallback.c
+++ src/Average_Fallback.c
@@ -53,15 +53,15 @@ struct Color cielab_avg_fallback_cw(const struct cielab_SoA *__restrict colors,
 
 	size_t i = 0;
 	for (; i < num_col; ++i) {
-		tmpa	= colors->a[i];
-		tmpb	= colors->b[i];
+		tmpa   = colors->a[i];
+		tmpb   = colors->b[i];
 		chroma = sqrtf(SQUARE(tmpa) + SQUARE(tmpb));
 		l += colors->l[i];
 		a += tmpa * chroma;
 		b += tmpb * chroma;
 		w += chroma;
 	}
-	
+
 	l /= num_col;
 	if (w == 0.0f) {
 		a = 0.0f;
@@ -75,26 +75,33 @@ struct Color cielab_avg_fallback_cw(const struct cielab_SoA *__restrict colors,
 }
 
 struct Color oklab_avg_fallback(const struct oklab_SoA *__restrict colors,
-				uint16_t num_col) {
-	float l, a, b;
-	l = a = b = 0.0f;
+				 uint16_t num_col) {
+	float l1, l2, l3, l4;
+	float a1, a2, a3, a4;
+	float b1, b2, b3, b4;
+	l1 = l2 = l3 = l4 = 0.0f;
+	a1 = a2 = a3 = a4 = 0.0f;
+	b1 = b2 = b3 = b4 = 0.0f;
+
 	for (size_t i = 0; i + 3 < num_col; i += 4) {
-		l += colors->l[i];
-		l += colors->l[i + 1];
-		l += colors->l[i + 2];
-		l += colors->l[i + 3];
+		l1 += colors->l[i];
+		l2 += colors->l[i + 1];
+		l3 += colors->l[i + 2];
+		l4 += colors->l[i + 3];
 
-		a += colors->a[i];
-		a += colors->a[i + 1];
-		a += colors->a[i + 2];
-		a += colors->a[i + 3];
+		a1 += colors->a[i];
+		a2 += colors->a[i + 1];
+		a3 += colors->a[i + 2];
+		a4 += colors->a[i + 3];
 
-		b += colors->b[i];
-		b += colors->b[i + 1];
-		b += colors->b[i + 2];
-		b += colors->b[i + 3];
+		b1 += colors->b[i];
+		b2 += colors->b[i + 1];
+		b3 += colors->b[i + 2];
+		b4 += colors->b[i + 3];
 	}
-
+	float l = l1 + l2 + l3 + l4;
+	float a = a1 + a2 + a3 + a4;
+	float b = b1 + b2 + b3 + b4;
 	l /= num_col;
 	a /= num_col;
 	b /= num_col;
@@ -102,7 +109,7 @@ struct Color oklab_avg_fallback(const struct oklab_SoA *__restrict colors,
 }
 
 struct Color oklab_avg_fallback_cw(const struct oklab_SoA *__restrict colors,
-				    uint16_t num_col) {
+				   uint16_t num_col) {
 	float l	     = 0.0f;
 	float a	     = 0.0f;
 	float b	     = 0.0f;
@@ -113,15 +120,15 @@ struct Color oklab_avg_fallback_cw(const struct oklab_SoA *__restrict colors,
 
 	size_t i = 0;
 	for (; i < num_col; ++i) {
-		tmpa	= colors->a[i];
-		tmpb	= colors->b[i];
+		tmpa   = colors->a[i];
+		tmpb   = colors->b[i];
 		chroma = sqrtf(SQUARE(tmpa) + SQUARE(tmpb));
 		l += colors->l[i + 0];
 		a += tmpa * chroma;
 		b += tmpb * chroma;
 		w += chroma;
 	}
-	
+
 	l /= num_col;
 	if (w == 0.0f) {
 		a = 0.0f;
--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "Align.h"
 #include "Average.h"
@@ -13,7 +14,7 @@
 /* TODO (Tess): Consider throwing GPU compute capabilities in here somewhere */
 
 #define RUNS pow(2, 22)
-#define RUNS_AVG pow(2, 15)
+#define RUNS_AVG pow(2, 9)
 #define NUM_TEST_COL 10000
 
 extern struct system_features_t features;
@@ -31,22 +32,25 @@ static inline int height_from_width(int width) {
 }
 
 int main(int argc, char **argv) {
+	clock_t tic = clock();
 	srand(0);
 	query_features(&features);
 
-	printf("Color is %zu bytes\n", sizeof(struct Color));
+	/* printf("Color is %" PRIu64 " bytes\n", sizeof(struct Color)); */
 
-	Color col1 = Color_create(rand() % 255, rand() % 255, rand() % 255);
-	Color col2 = Color_create(rand() % 255, rand() % 255, rand() % 255);
+	/* Color col1 = Color_create(rand() % 255, rand() % 255, rand() % 255);
+	 */
+	/* Color col2 = Color_create(rand() % 255, rand() % 255, rand() % 255);
+	 */
 
-	printf("Delta ok took %lu cycles on average.\n",
-	       time_diff(delta_ok_diff, &col1, &col2, RUNS));
-	printf("Delta cie76 took %lu cycles on average.\n",
-	       time_diff(delta_cie76_diff, &col1, &col2, RUNS));
-	printf("Delta cie94 took %lu cycles on average.\n",
-	       time_diff(delta_cie94_diff, &col1, &col2, RUNS));
-	printf("Delta ciede2000 took %lu cycles on average.\n",
-	       time_diff(delta_ciede2000_diff, &col1, &col2, RUNS));
+	/* printf("Delta ok took %" PRIu64 " cycles on average.\n", */
+	/*        time_diff(delta_ok_diff, &col1, &col2, RUNS)); */
+	/* printf("Delta cie76 took %" PRIu64 " cycles on average.\n", */
+	/*        time_diff(delta_cie76_diff, &col1, &col2, RUNS)); */
+	/* printf("Delta cie94 took %" PRIu64 " cycles on average.\n", */
+	/*        time_diff(delta_cie94_diff, &col1, &col2, RUNS)); */
+	/* printf("Delta ciede2000 took %" PRIu64 " cycles on average.\n", */
+	/*        time_diff(delta_ciede2000_diff, &col1, &col2, RUNS)); */
 
 	/* printf("%" PRIu64 " bytes of l1 cache\n", features.l[1]); */
 	/* printf("%" PRIu64 " bytes of l2 cache\n", features.l[2]); */
@@ -93,66 +97,67 @@ int main(int argc, char **argv) {
 	/*        "must be shrunk to ~%dx%d\n", */
 	/*        cache, pixels, width, height); */
 
-	/* struct Video in_video; */
-	/* open_video_file(&in_video, "vid.webm"); */
-
 	struct Color test_colors[NUM_TEST_COL] = {};
 	for (size_t i = 0; i < NUM_TEST_COL; ++i) {
 		test_colors[i] =
 		    Color_create(rand() % 255, rand() % 255, rand() % 255);
 		convert_to(&test_colors[i], COLOR_CIELAB);
 	}
-	
+
 	cielab_SoA ciesoa32;
 	ciesoa32.l = aligned_malloc(32, NUM_TEST_COL * sizeof(float));
 	ciesoa32.a = aligned_malloc(32, NUM_TEST_COL * sizeof(float));
 	ciesoa32.b = aligned_malloc(32, NUM_TEST_COL * sizeof(float));
 
 	for (size_t i = 0; i < NUM_TEST_COL; ++i) {
-	  ciesoa32.l[i] = test_colors[i].data.cielab.l;
-	  ciesoa32.a[i] = test_colors[i].data.cielab.a;
-	  ciesoa32.b[i] = test_colors[i].data.cielab.b;
+		ciesoa32.l[i] = test_colors[i].data.cielab.l;
+		ciesoa32.a[i] = test_colors[i].data.cielab.a;
+		ciesoa32.b[i] = test_colors[i].data.cielab.b;
 	}
+	const char *header =
+	    "NAME\t\t\tCW\t# OF COLORS\t\tAVG. CYCLES\t\tAVG. CYCLES/COLOR";
+	const char *tmp_msg =
+	    "%s\t\t%s\t%" PRIu16 "\t\t\t%" PRIu64 "\t\t\t%.5f";
+	char act_msg[256];
 
-	printf("==========================================================\n");
-	printf("========================= CIELAB =========================\n");
-	printf("==========================================================\n");
+	printf("=================================================================================================\n");
+	printf("============================================= CIELAB ============================================\n");
+	printf("=================================================================================================\n");
 	uint64_t avg;
-	avg = time_avg_cie(cielab_avg_fallback, &ciesoa32, NUM_TEST_COL, RUNS_AVG);
-	printf("Fallback averaging %" PRIu16
-	       " colors took %lu cycles on average, or about %.3f "
-	       "cycles per color on average.\n",
-	       NUM_TEST_COL, avg, (double)avg / (double)NUM_TEST_COL);
-	
-	avg = time_avg_cie(cielab_avg_fallback_cw, &ciesoa32, NUM_TEST_COL, RUNS_AVG);
-	printf("Fallback averaging w/ CW %" PRIu16
-	       " colors took %lu cycles on average, or about %.3f cycles per "
-	       "color on average.\n",
-	       NUM_TEST_COL, avg, (double)avg / (double)NUM_TEST_COL);
-	
+	puts(header);
+	puts("-----------------------------------------------------------------"
+	     "--------------------------------");
+	avg = time_avg_cie(cielab_avg_fallback, &ciesoa32, NUM_TEST_COL,
+			   RUNS_AVG);
+	sprintf(act_msg, tmp_msg, "Fallback", "No", NUM_TEST_COL, avg,
+		(double)avg / (double)NUM_TEST_COL);
+	puts(act_msg);
+	avg = time_avg_cie(cielab_avg_fallback_cw, &ciesoa32, NUM_TEST_COL,
+			   RUNS_AVG);
+	sprintf(act_msg, tmp_msg, "Fallback", "Yes", NUM_TEST_COL, avg,
+		(double)avg / (double)NUM_TEST_COL);
+	puts(act_msg);
 	avg = time_avg_cie(cielab_avg_sse, &ciesoa32, NUM_TEST_COL, RUNS_AVG);
-	printf("SSE averaging %" PRIu16
-	       " colors took %lu cycles on average, or about %.3f "
-	       "cycles per color on average.\n",
-	       NUM_TEST_COL, avg, (double)avg / (double)NUM_TEST_COL);
-	
-	avg = time_avg_cie(cielab_avg_sse_cw, &ciesoa32, NUM_TEST_COL, RUNS_AVG);
-	printf("SSE averaging w/ CW %" PRIu16
-	       " colors took %lu cycles on average, or about %.3f "
-	       "cycles per color on average.\n",
-	       NUM_TEST_COL, avg, (double)avg / (double)NUM_TEST_COL);
-	
+	sprintf(act_msg, tmp_msg, "SSE     ", "No", NUM_TEST_COL, avg,
+		(double)avg / (double)NUM_TEST_COL);
+	puts(act_msg);
+
+	avg =
+	    time_avg_cie(cielab_avg_sse_cw, &ciesoa32, NUM_TEST_COL, RUNS_AVG);
+	sprintf(act_msg, tmp_msg, "SSE     ", "Yes", NUM_TEST_COL, avg,
+		(double)avg / (double)NUM_TEST_COL);
+	puts(act_msg);
+
 	avg = time_avg_cie(cielab_avg_avx, &ciesoa32, NUM_TEST_COL, RUNS_AVG);
-	printf("AVX averaging %" PRIu16
-	       " colors took %lu cycles on average, or about %.3f "
-	       "cycles per color on average.\n",
-	       NUM_TEST_COL, avg, (double)avg / (double)NUM_TEST_COL);
-	
-	avg = time_avg_cie(cielab_avg_avx_cw, &ciesoa32, NUM_TEST_COL, RUNS_AVG);
-	printf("AVX averaging w/ CW %" PRIu16
-	       " colors took %lu cycles on average, or about %.3f "
-	       "cycles per color on average.\n",
-	       NUM_TEST_COL, avg, (double)avg / (double)NUM_TEST_COL);
+	sprintf(act_msg, tmp_msg, "AVX     ", "No", NUM_TEST_COL, avg,
+		(double)avg / (double)NUM_TEST_COL);
+	puts(act_msg);
+
+	avg =
+	    time_avg_cie(cielab_avg_avx_cw, &ciesoa32, NUM_TEST_COL, RUNS_AVG);
+	sprintf(act_msg, tmp_msg, "AVX     ", "Yes", NUM_TEST_COL, avg,
+		(double)avg / (double)NUM_TEST_COL);
+	puts(act_msg);
 
 	aligned_free(ciesoa32.l);
 	aligned_free(ciesoa32.a);
@@ -164,54 +169,52 @@ int main(int argc, char **argv) {
 	oksoa32.b = aligned_malloc(32, NUM_TEST_COL * sizeof(float));
 
 	for (size_t i = 0; i < NUM_TEST_COL; ++i) {
-	  oksoa32.l[i] = test_colors[i].data.oklab.l;
-	  oksoa32.a[i] = test_colors[i].data.oklab.a;
-	  oksoa32.b[i] = test_colors[i].data.oklab.b;
+		oksoa32.l[i] = test_colors[i].data.oklab.l;
+		oksoa32.a[i] = test_colors[i].data.oklab.a;
+		oksoa32.b[i] = test_colors[i].data.oklab.b;
 	}
 
-	printf("=======================================================\n");
-	printf("======================== OKLAB ========================\n");
-	printf("=======================================================\n");
-	
+	printf("=================================================================================================\n");
+	printf("============================================= OKLAB =============================================\n");
+	printf("=================================================================================================\n");
+
+	puts(header);
+	puts("-----------------------------------------------------------------"
+	     "--------------------------------");
 	avg = time_avg_ok(oklab_avg_fallback, &oksoa32, NUM_TEST_COL, RUNS_AVG);
-	printf("Fallback averaging %" PRIu16
-	       " colors took %lu cycles on average, or about %.3f "
-	       "cycles per color on average.\n",
-	       NUM_TEST_COL, avg, (double)avg / (double)NUM_TEST_COL);
-	
-	avg = time_avg_ok(oklab_avg_fallback_cw, &oksoa32, NUM_TEST_COL, RUNS_AVG);
-	printf("Fallback averaging w/ CW %" PRIu16
-	       " colors took %lu cycles on average, or about %.3f cycles per "
-	       "color on average.\n",
-	       NUM_TEST_COL, avg, (double)avg / (double)NUM_TEST_COL);
-	
+	sprintf(act_msg, tmp_msg, "Fallback", "No", NUM_TEST_COL, avg,
+		(double)avg / (double)NUM_TEST_COL);
+	puts(act_msg);
+	avg = time_avg_ok(oklab_avg_fallback_cw, &oksoa32, NUM_TEST_COL,
+			  RUNS_AVG);
+	sprintf(act_msg, tmp_msg, "Fallback", "Yes", NUM_TEST_COL, avg,
+		(double)avg / (double)NUM_TEST_COL);
+	puts(act_msg);
 	avg = time_avg_ok(oklab_avg_sse, &oksoa32, NUM_TEST_COL, RUNS_AVG);
-	printf("SSE averaging %" PRIu16
-	       " colors took %lu cycles on average, or about %.3f "
-	       "cycles per color on average.\n",
-	       NUM_TEST_COL, avg, (double)avg / (double)NUM_TEST_COL);
-	
+	sprintf(act_msg, tmp_msg, "SSE     ", "No", NUM_TEST_COL, avg,
+		(double)avg / (double)NUM_TEST_COL);
+	puts(act_msg);
+
 	avg = time_avg_ok(oklab_avg_sse_cw, &oksoa32, NUM_TEST_COL, RUNS_AVG);
-	printf("SSE averaging w/ CW %" PRIu16
-	       " colors took %lu cycles on average, or about %.3f "
-	       "cycles per color on average.\n",
-	       NUM_TEST_COL, avg, (double)avg / (double)NUM_TEST_COL);
-	
+	sprintf(act_msg, tmp_msg, "SSE     ", "Yes", NUM_TEST_COL, avg,
+		(double)avg / (double)NUM_TEST_COL);
+	puts(act_msg);
+
 	avg = time_avg_ok(oklab_avg_avx, &oksoa32, NUM_TEST_COL, RUNS_AVG);
-	printf("AVX averaging %" PRIu16
-	       " colors took %lu cycles on average, or about %.3f "
-	       "cycles per color on average.\n",
-	       NUM_TEST_COL, avg, (double)avg / (double)NUM_TEST_COL);
-	
+	sprintf(act_msg, tmp_msg, "AVX     ", "No", NUM_TEST_COL, avg,
+		(double)avg / (double)NUM_TEST_COL);
+	puts(act_msg);
+
 	avg = time_avg_ok(oklab_avg_avx_cw, &oksoa32, NUM_TEST_COL, RUNS_AVG);
-	printf("AVX averaging w/ CW %" PRIu16
-	       " colors took %lu cycles on average, or about %.3f "
-	       "cycles per color on average.\n",
-	       NUM_TEST_COL, avg, (double)avg / (double)NUM_TEST_COL);
+	sprintf(act_msg, tmp_msg, "AVX     ", "Yes", NUM_TEST_COL, avg,
+		(double)avg / (double)NUM_TEST_COL);
+	puts(act_msg);
 
 	aligned_free(oksoa32.l);
 	aligned_free(oksoa32.a);
 	aligned_free(oksoa32.b);
-
+	clock_t toc = clock();
+	double ms = (double)(toc - tic) / CLOCKS_PER_SEC;
+	printf("Total ms: %f\n", ms);
 	return 0;
 }
